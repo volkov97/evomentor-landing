22ee75eda484d54a6f5dabdbcde3b429f5736d13
volkov97 <Volkov.german.1997@gmail.com>
2017-10-11 11:25:53 +0300

    clean from nextjs

---
+++ _static/sw.js
@@ -0,0 +1,32 @@
+// we'll version our cache (and learn how to delete caches in
+// some other post)
+const cacheName = 'v1::static';
+
+self.addEventListener('install', (e) => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll([
+      '/',
+      /*
+        DEAR READER,
+        ADD A LIST OF YOUR ASSETS THAT
+        YOU WANT TO WORK WHEN OFFLINE
+        TO THIS ARRAY OF URLS
+      */
+    ]).then(() => self.skipWaiting())),
+  );
+});
+
+// when the browser fetches a url, either response with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(cacheName).then(cache =>
+      cache.match(event.request).then(res =>
+        res || fetch(event.request),
+      ),
+    ),
+  );
+});
